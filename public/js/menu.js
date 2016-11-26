644e927266754ca409178d98cf01c02c77737950
Federico <federico.fiorini29@gmail.com>
2016-11-26 22:50:12 +0200

    dynamic busses

--- public/js/menu.js
+++ public/js/menu.js
@@ -6,8 +6,35 @@ $(function() {
   var menu4_clicked = false;
 
   $( "#menu-001" ).click(function(event) {
-		// $(this).attr("src", "/public/img/content_001.png");
     parent.postMessage("restartTimer", "*");
+
+    if (menu1_clicked == true) {
+      return;
+    }
+
+    menu1_clicked = true;
+    $("#bus-frame")
+      .css('z-index', '+=10')
+      .css('visibility', 'visible')
+      .animate({
+			    opacity: 1,
+			    width: "100%",
+			    height: "100%"
+			}, 1000, function() {}
+		);
+
+    setTimeout(function() {
+      $("#bus-frame").animate({
+  			    opacity: 1,
+  			    width: "50%",
+  			    height: "50%"
+  			}, 1000, function() {
+          $(this).css('z-index', '-=10')
+          .css('visibility', 'hidden');
+          menu1_clicked = false;
+        }
+  		)
+    }, 4000);
 	});
 
   $( "#menu-002" ).click(function(event) {
@@ -45,11 +72,11 @@ $(function() {
   $( "#menu-003" ).click(function(event) {
     parent.postMessage("restartTimer", "*");
 
-    if (menu2_clicked == true) {
+    if (menu3_clicked == true) {
       return;
     }
 
-    menu2_clicked = true;
+    menu3_clicked = true;
 		$(this).attr("src", "/public/img/content_003.png");
     $(this).css('z-index', '+=10')
       .animate({
@@ -67,7 +94,7 @@ $(function() {
   			}, 1000, function() {
           $(this).css('z-index', '-=10');
           $(this).attr("src", "/public/img/menu_003.png");
-          menu2_clicked = false;
+          menu3_clicked = false;
         }
   		)
     }, 4000);
@@ -76,11 +103,11 @@ $(function() {
   $( "#menu-004" ).click(function(event) {
     parent.postMessage("restartTimer", "*");
 
-    if (menu2_clicked == true) {
+    if (menu4_clicked == true) {
       return;
     }
 
-    menu2_clicked = true;
+    menu4_clicked = true;
 		$(this).attr("src", "/public/img/content_004.png");
     $(this).css('z-index', '+=10')
       .animate({
@@ -98,7 +125,7 @@ $(function() {
   			}, 1000, function() {
           $(this).css('z-index', '-=10');
           $(this).attr("src", "/public/img/menu_004.png");
-          menu2_clicked = false;
+          menu4_clicked = false;
         }
   		)
     }, 4000);
