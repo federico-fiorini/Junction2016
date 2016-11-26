02c122151600ee68758650df22246cf8a305fd5f
Teodor <me@teodorpatras.com>
2016-11-27 00:37:21 +0200

    fixes

--- public/js/snap.js
+++ public/js/snap.js
@@ -68,13 +68,9 @@ function applyEmojis(status) {
     for (i = 0; i < status.length; i++) {
         var img = document.createElement('img')
         var dict = status[i]
-<<<<<<< HEAD
         img.src = `/public/img/${dict.gender}_${dict.emotion}.png`
         img.className = `emojy-img`
         console.log(img.url)
-=======
-        img.url = `/public/img/${dict.gender}_${dict.emotion}.png`
->>>>>>> 9552ac8956284b9ee5b61652f8c6fe83a5282666
         var textDiv = document.createElement('div')
         textDiv.innerHTML = `${dict.age}`
         var statusDiv = document.createElement('div')
@@ -139,61 +135,3 @@ function post(data, completion) {
     }
     xhr.send(data)
 }
-
-/*
-function analyzeEmotion(data, completion) {
-    const key = '63609cc73858437f9cadcd3e503c25b9'
-    const url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
-    post(url, data, key, completion)
-}
-
-function analyzeFace(data, completion) {
-    const key = '21a3cbe1e9c74b6882f0a21e02477fd4'
-    const url = 'https://api.projectoxford.ai/face/v1.0/detect?returnFaceId=true&returnFaceAttributes=age,gender,headPose'
-    post(url, data, key, completion)
-}
-
-function post(url, data, key, completion) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url)
-    xhr.setRequestHeader('Content-Type', 'application/octet-stream')
-    xhr.setRequestHeader('Ocp-Apim-Subscription-Key', key)
-
-    xhr.onload = function() {
-        completion(JSON.parse(this.response))
-    }
-
-    xhr.send(data)
-}
-<<<<<<< HEAD
-=======
-*/
->>>>>>> 9552ac8956284b9ee5b61652f8c6fe83a5282666
-
-function b64toBlob(b64Data, contentType, sliceSize) {
-    contentType = contentType || '';
-    sliceSize = sliceSize || 512;
-
-    var byteCharacters = atob(b64Data);
-    var byteArrays = [];
-
-    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        var byteArray = new Uint8Array(byteNumbers);
-
-        byteArrays.push(byteArray);
-    }
-
-    var blob = new Blob(byteArrays, { type: contentType });
-    return blob;
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 9552ac8956284b9ee5b61652f8c6fe83a5282666
